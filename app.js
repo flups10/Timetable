7a754b39e6de83174ece00cdb88faf87ef6519d6
Joris <je.tieleman@gmail.com>
2022-11-11 10:23:21 +0100

    initial commit, basic setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const ejs = require('ejs')
+const mongo = require('mongoose')
+const fs = require('fs')
+
+app = express()
+
+app.use(express.static('public'))
+
+// This way I keep the DB URI a secret
+mongoURL = fs.readFile('secret.txt', 'utf8', (err, data) => {
+    mongo.connect(data)
+        .then(() => app.listen(3001), console.log('succes'))
+        .catch(err => console.log(err))
+
+})
+
+app.set('view engine','ejs')
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
